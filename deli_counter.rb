75b632bd0f94f6d4f62ad929a9a0187dea842767
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-02 20:58:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+
+
+def take_a_number(array, string)
+  blank = []
+  array << string
+  array.each_with_index do |people, index|
+    blank << "Welcome, #{people}. You are number #{index + 1} in line."
+  end
+  blank
+end
+take_a_number(['ana', 'david', 'charles'], 'bob')
