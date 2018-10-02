598df15713bc42868871abbfa6a43880186bc971
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-02 21:15:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def take_a_number(array, string)
   array.each_with_index do |people, index|
     blank << "Welcome, #{people}. You are number #{index + 1} in line."
   end
-  blank
+  puts blank
 end
 take_a_number(['ana', 'david', 'charles'], 'bob')
